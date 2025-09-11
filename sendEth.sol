8cb50bdb1d649c2b83b1bfc3b660ff0d2fc0fa4b
Sawiru <sawirudw@gmail.com>
2025-09-11 12:43:43 +0530

    New updates

--- sendEth.sol
+++ sendEth.sol
@@ -5,21 +5,31 @@ pragma solidity ^0.8.9;
 // transfer - 2300 gas, reverts
 // send - 2300 gas, returns bool
 // call - all gas, returns bool and data
-
+ 
 contract SendEther {
     constructor() payable {}
 
-    recieve() external payable {}
+    receive() external payable {}
 
     function sendViaTransfer(address payable _to) external payable {
-
+        _to.transfer(123);
     }
 
     function sendViaSend(address payable _to) external payable {
-
+        bool sent = _to.send(123);
+        require(sent, "send failed");
     }
 
     function sendViaCall(address payable _to) external payable {
-        
+        (bool success, ) = _to.call{value: 123}("");
+        require(success, "call failed");
     }
+}
+
+contract EthReceiver {
+    event Log(uint amount, uint gas);
+
+    receive() external payable {
+        emit Log(msg.value, gasleft());
+     }
 }
